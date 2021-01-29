0709f00858e93b8b3cbfca1e3cec8edd61351e62
kzvin <74388378+kzvin@users.noreply.github.com>
2021-01-29 20:25:20 -0300

    Create juego.js

---
+++ juego.js
@@ -0,0 +1,25 @@
+document.addEventListener('keydown', function(evento){
+if(evento.keyCode == 32){
+console.log("Que haces viendo la consola? Quieres que te robe tu computadora? :)")
+}
+});
+
+
+
+var ancho = 700;
+var alto = 300;
+
+
+function borraCanvas(){
+ canvas.width =
+}
+
+//Bucle principal
+var FPS = 10;
+setInterval(function(){
+ principal();
+},1000/10);
+
+function principal(){
+
+}
